b58ee0ff4e5175593f5eb10f30deaf3bb5b1639d
amey-babhulgaonkar <amey.babhulgaonkar123@gmail.com>
2025-11-09 16:11:04 +0530

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n ;
+     printf("Enter The Number Of Rows You Want In The Pattern\n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+       
+        for (int s = 1; s <= n - i; s++) {
+            printf(" ");
+        }
+       
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
